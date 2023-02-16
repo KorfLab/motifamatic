7b0afcff4d5e82adb746a3aaad7f37cfb6a9b824
iankorf <ifkorf@gmail.com>
2023-02-15 21:39:15 -0800

    up

--- motiflib.py
+++ motiflib.py
@@ -37,20 +37,20 @@ def seqs2motif(seqs):
 	for i in range(len(seqs)):
 		for j in range(len(seqs[i])):
 			count[j][seqs[i][j]] += 1
-	
+
 	freq = []
 	for i in range(len(count)):
 		f = {}
 		for nt in count[i]: f[nt] = count[i][nt] / len(seqs)
 		freq.append(f)
-		
+
 	return freq
 
-def yscale(col):
+def entropy(col):
 	h = 0
 	for nt in col:
 		if col[nt] != 0: h -= col[nt] * math.log2(col[nt])
-	return 2 - h
+	return h
 
 
 def motif2svg(motif):
@@ -64,16 +64,16 @@ def motif2svg(motif):
 
 	W = len(motif) * 30
 	H = 100
-	
+
 	# header
 	style = '<style>.sm {font:10px sans-serif;} .lg {font:70px sans-serif;}</style>'
 	xmlns = 'xmlns="http://www.w3.org/2000/svg"'
 	vbox = f'viewBox="0 0 {W} {H}'
-	
+
 	svg = []
 	svg.append(f'<svg {vbox} {xmlns}>')
 	svg.append(style)
-	
+
 	# y-axis
 	x1 = 0
 	y1 = 0
@@ -81,7 +81,7 @@ def motif2svg(motif):
 	y2 = H
 	sk = 'stroke="black"'
 	svg.append(f'<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" {sk}/>')
-	
+
 	ty = [0, 25, 50, 75, 100]
 	x1 = 0
 	x2 = 2
@@ -90,14 +90,15 @@ def motif2svg(motif):
 		y1 = y
 		y2 = y
 		svg.append(f'<line x1="{x1}" y1="{y1}" x2="{x2}" y2="{y2}" {sk}/>')
-	
+
 	# letters
 	for i, col in enumerate(motif):
-		ys = yscale(col)
+		ys = 2- entropy(col)
+		if ys == 0: continue
 		yoff = 0 # percent of pixels already used
 		xoff = i * 46 + 30
 		for nt, p in sorted(col.items(), key=lambda item: item[1]):
-			if p == 0: continue			
+			if p == 0: continue
 			yp = p * ys # this nt proportion of scale
 			c = color[nt]
 			a = 'text-anchor="middle"'
@@ -105,17 +106,17 @@ def motif2svg(motif):
 			s = 'class="lg"'
 			x = f'x="{xoff}"'
 			y0 = H / yp # zero position
-			yd = yoff * H / yp # y-delta, not quite correct
+			yd = 0.5 * yoff * H / yp # y-delta, not quite correct
 			y = f'y="{y0 - yd}"'
-			
+
 			print(f'{ys:.3f} {nt} {p:.3f} {yp:.4f} {yoff:.3f}',
 				file=sys.stderr)
-			
+
 			svg.append(f'<g {c} {t}><text {a} {s} {x} {y}>{nt}</text></g>')
-			yoff += p
+			yoff += yp
 		print(file=sys.stderr)
-	
+
 	# footer
 	svg.append('</svg>\n')
-	
+
 	return '\n'.join(svg)
