48e1b9b43105c7eea3789ee1d75f95d870525090
iankorf <ifkorf@gmail.com>
2023-02-17 08:59:49 -0800

    up

--- test.py
+++ test.py
@@ -2,5 +2,9 @@ import sys
 import motiflib
 import json
 
-m = motiflib.PWM('motif.fasta')
+m = motiflib.PWM('motif.fasta', name='test', source='fabricated')
+print(m.name)
+print(m.source)
+print(m.length)
+print(m.entropy)
 print(m)
