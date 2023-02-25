1a9de8bf5a2fcc576ef4991d5a1889c1b4b0814d
iankorf <ifkorf@gmail.com>
2023-02-25 09:23:40 -0800

    up

--- tests/test_math.py
+++ tests/test_math.py
@@ -3,6 +3,10 @@ import math
 
 import motiflib
 
+def test_check_probs_ok():
+	p = [0.4, 0.3, 0.2, 0.1]
+	motiflib.check_probs(p)
+
 def test_checkprobs_negative():
 	p = [1.0, -1.0]
 	with pytest.raises(AssertionError):
@@ -13,17 +17,10 @@ def test_checkprobs_sum():
 	with pytest.raises(AssertionError):
 		motiflib.check_probs(p)
 
-def test_entropy1():
-	p = [0.5, 0.5]
-	assert(math.isclose(1.0, motiflib.entropy(p)))
-
-def test_entropy2():
-	p = [0.25, 0.25, 0.25, 0.2500001] # does not quite sum to 1, ok
-	assert(math.isclose(2.0, motiflib.entropy(p), abs_tol=1e-3))
-
 """
 Tests to add
-dl1() Jenna
-dl2() Sai
-dkl() Claire
+entropy() has no tests: Meghana
+dl1() has no tests: Jenna
+dl2() has no tests: Sai
+dkl() has no tests: Claire
 """
